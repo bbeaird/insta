2bfedd6a8718a97f5f7f87fa769a435ad28189a5
Brantley Beaird <bbeaird@gmail.com>
2015-10-28 16:35:20 -0700

    commented out the wrong line...

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,7 @@ class User < ActiveRecord::Base
 
   # Sends activation email.
   def send_activation_email
-    UserMailer.account_activation(self).deliver_now
+    # UserMailer.account_activation(self).deliver_now
   end
 
   # Sets the password reset attributes.
@@ -69,7 +69,7 @@ class User < ActiveRecord::Base
 
   # Sends password reset email.
   def send_password_reset_email
-    # UserMailer.password_reset(self).deliver_now
+    UserMailer.password_reset(self).deliver_now
   end
 
   # Returns true if a password reset has expired.
