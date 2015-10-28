814fbe6325b55c5ac5fb54ee29e5713f95274949
Brantley Beaird <bbeaird@gmail.com>
2015-10-28 16:27:33 -0700

    commented out sending of activation email.

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,8 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+analytics.identify('f4ca124298', {
+  email: $('#session_email')[0].value
+});
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -69,7 +69,7 @@ class User < ActiveRecord::Base
 
   # Sends password reset email.
   def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
+    # UserMailer.password_reset(self).deliver_now
   end
 
   # Returns true if a password reset has expired.
