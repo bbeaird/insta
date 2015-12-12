a4dd4d64718c392797ce7222b8662ef188027204
Brantley Beaird <bbeaird@gmail.com>
2015-12-12 14:30:45 -0800

    Changed routes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111214811) do
+ActiveRecord::Schema.define(version: 20151212221751) do
+
+  create_table "instathreat_users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "phone_number"
+    t.string   "why_owed_money"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
