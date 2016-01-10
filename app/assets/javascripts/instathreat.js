3bffbd2b6f98e435a50bc47e881090ae885d6263
Brantley Beaird <bbeaird@gmail.com>
2016-01-10 00:22:13 -0800

    Added identify call.

--- app/assets/javascripts/instathreat.js
+++ app/assets/javascripts/instathreat.js
@@ -1,25 +1,13 @@
 <head>
-<!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','//connect.facebook.net/en_US/fbevents.js');
 
-fbq('init', '1100957839928916');
-fbq('track', "PageView");</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1100957839928916&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Facebook Pixel Code -->
-
+<!-- Segment Code Start -->
 <script type="text/javascript">
 !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
 analytics.load("WZkmDkZtSmrlEe8sO1b3QI9djuNiPcja");
 analytics.page()
 }}();
 </script>
+<!-- Segment Code End -->
 
 </head>
 
@@ -33,16 +21,27 @@ var handler = StripeCheckout.configure({
   token: function(token) {
     // Use the token to create the charge with a server-side script.
     // You can access the token ID with `token.id`
-    fbq('track', 'Purchase', {value: '49.00', currency: 'USD'});
+    // fbq('track', 'Purchase', {value: '49.00', currency: 'USD'});
+    var name = $('input')[1].value
+    var email = $('input')[2].value;
+    var phoneNumber = $('input')[3].value;
+    analytics.identify(email, {
+      name: name,
+      email: email,
+      phoneNumber: phoneNumber
+    })
     analytics.track('Completed Order');
+    console.log('Completed Order');
     console.log('Stripe completion callback!');
   }
 });
 
 var form = document.getElementsByTagName('form')[0]
 window.instapageFormSubmitSuccess = function(form){
+  analytics.track('Form Submitted');
+  console.log('Form Submitted');
   handler.open({
-    name: 'Instathreats',
+    name: 'InstaThreats',
     description: '100% money back guarantee',
     // description: "Get the money you're owed"
     amount: 4900
